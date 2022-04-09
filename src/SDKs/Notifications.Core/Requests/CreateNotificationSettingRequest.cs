3920aeaf6eedf2aa7247c30e742cdf542a615691
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-04-09 23:59:31 +0200

    Created new SDK

---
+++ src/SDKs/Notifications.Core/Requests/CreateNotificationSettingRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Notifications.Core.Requests;
+
+public record CreateNotificationSettingRequest
+{
+    public string Channel { get; set; } = default!;
+
+    public bool IsOptIn { get; set; }
+}
---
+++ src/SDKs/Notifications.Core/Requests/CreateUserRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Notifications.Core.Requests;
+
+public record CreateUserRequest
+{
+    public string Name { get; init; } = default!;
+
+    public string EmailAddress { get; init; } = default!;
+
+    public string PhoneNumber { get; init; } = default!;
+
+    public CreateNotificationSettingRequest Settings { get; init; } = default!;
+}
