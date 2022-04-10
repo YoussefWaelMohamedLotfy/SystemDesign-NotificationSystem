34f493237d996f28677561e4e1d7e4bbe04241a4
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-04-10 03:50:11 +0200

    Added DELETE Endpoint

---
+++ src/Services/Notifications.API/Endpoints/V1/Users/Delete.cs
@@ -0,0 +1,29 @@
+﻿using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Mvc;
+using Notifications.API.Data.Repository;
+
+namespace Notifications.API.Endpoints.V1.Users;
+
+public class Delete : EndpointBaseAsync.WithRequest<int>.WithActionResult
+{
+    private readonly IUserRepository _userRepository;
+
+    public Delete(IUserRepository userRepository)
+    {
+        _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+    }
+
+    [HttpDelete("/api/[namespace]/{id}")]
+    public override async Task<ActionResult> HandleAsync(int id, CancellationToken cancellationToken = default)
+    {
+        var user = await _userRepository.GetUserById(id, cancellationToken);
+
+        if (user is null)
+            return NotFound();
+
+        _userRepository.Delete(user);
+        await _userRepository.CompleteChangesAsync(cancellationToken);
+
+        return NoContent();
+    }
+}
---
+++ test/Services/Notifications.API.Tests/EndpointTests/V1/Users/DeleteTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Notifications.API.Data.Repository;
+using Notifications.API.Endpoints.V1.Users;
+using Notifications.API.Models;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace Notifications.API.Tests.EndpointTests.V1.Users;
+
+[TestFixture]
+public class DeleteTests
+{
+    private Delete DELETEendpoint = default!;
+    private Mock<IUserRepository> _userRepositoryMock;
+
+
+    [SetUp]
+    public void Setup()
+    {
+        _userRepositoryMock = new();
+        DELETEendpoint = new(_userRepositoryMock.Object);
+    }
+
+    [Test]
+    public async Task ExecuteDelete_EnterUserID_UserDeletedAndReturnsNoContent()
+    {
+        User testUser = new() { ID = 5 };
+        _userRepositoryMock.Setup(x => x.GetUserById(5, default))
+            .ReturnsAsync(testUser);
+
+        var response = await DELETEendpoint.HandleAsync(5) as NoContentResult;
+
+        Assert.Multiple(() =>
+        {
+            _userRepositoryMock.Verify(x => x.GetUserById(5, default), Times.Once());
+            _userRepositoryMock.Verify(x => x.Delete(testUser), Times.Once());
+            _userRepositoryMock.Verify(x => x.CompleteChangesAsync(default), Times.Once());
+
+            Assert.That(response, Is.Not.Null);
+            Assert.That(response, Is.InstanceOf<NoContentResult>());
+        });
+    }
+
+    [Test]
+    public async Task ExecuteDelete_EnterUserID_UserIsNullAndReturnsNotFound()
+    {
+        User testUser = new() { ID = 50 };
+        _userRepositoryMock.Setup(x => x.GetUserById(50, default))
+            .ReturnsAsync(() => null);
+
+        var response = await DELETEendpoint.HandleAsync(50);
+
+        Assert.Multiple(() =>
+        {
+            _userRepositoryMock.Verify(x => x.GetUserById(50, default), Times.Once());
+            _userRepositoryMock.Verify(x => x.Delete(testUser), Times.Never());
+            _userRepositoryMock.Verify(x => x.CompleteChangesAsync(default), Times.Never());
+
+            Assert.That(response, Is.Not.Null);
+            Assert.That(response, Is.InstanceOf<NotFoundResult>());
+        });
+    }
+
+    [Test]
+    public void ExecuteDelete_UserRepoIsNull_ReturnArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() => DELETEendpoint = new(null!));
+    }
+}
